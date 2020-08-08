8b057f0748ea104cf265804b68915fac5d774989
Artem Streltov <arstrel@gmail.com>
2020-08-08 13:24:27 -0400

    Iteration 1: identified issues

--- build/index.js
+++ build/index.js
@@ -4,6 +4,15 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 var fs_1 = __importDefault(require("fs"));
+/*
+Issues:
+1. Magic strings comparisons
+2. Source of data is hardcoded
+3. Data array is all strings, even though it might have numbers in it
+4. Variable named after a specific team
+5. Analisis type is fixed
+6. No ability to output the report in different formats
+*/
 var matches = fs_1.default
     .readFileSync('football.csv', {
     encoding: 'utf-8',
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,15 @@
 import fs from 'fs';
 
+/*
+Issues:
+1. Magic strings comparisons
+2. Source of data is hardcoded
+3. Data array is all strings, even though it might have numbers in it
+4. Variable named after a specific team
+5. Analisis type is fixed
+6. No ability to output the report in different formats
+*/
+
 const matches = fs
   .readFileSync('football.csv', {
     encoding: 'utf-8',
